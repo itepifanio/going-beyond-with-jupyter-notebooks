266f62f24a669ae879b00980f17a52c0434e47e4
Ítalo Epifânio <italoepifaniols@gmail.com>
2023-04-06 09:42:53 -0300

    Improve minimal package and add it as example

---
+++ examples/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='minimal_package',
+    version='0.0.1',
+    packages=['minimal_package']
+)
+
--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setuptools.setup(
         'Natural Language :: ' + cfg['language'].title(),
     ] + ['Programming Language :: Python :: '+o for o in py_versions[py_versions.index(min_python):]] + (['License :: ' + lic[1] ] if lic[1] else []),
     url = cfg['git_url'],
-    packages = setuptools.find_packages(),
+    packages = setuptools.find_packages(include=["mydeck"]),
     include_package_data = True,
     install_requires = requirements,
     extras_require={ 'dev': dev_requirements },
